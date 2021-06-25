c6abe547340763e8e026a51cd0baeef46472e6ac
VikramTheBlackDragoon <vikramdesmoines@gmail.com>
2021-06-25 17:32:59 +0530

    Card Validator modified

--- helpers/CardValidator.js
+++ helpers/CardValidator.js
@@ -1,9 +1,12 @@
 const Joi = require('joi');
 
 class CardValidator{
+    /**
+     * Schema for card to validate.
+     */
     static cardSchema(){
         const schema = Joi.object({
-            card_name: Joi.string().min(2).max(10).required(),
+            card_name: Joi.string().min(2).max(30).required(),
             card_no : Joi.number().min(111111111111).required(),
             expiry_month: Joi.number().min(1).max(12).required(),
             expiry_year: Joi.number().min(21).max(50).required()
